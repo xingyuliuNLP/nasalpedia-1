adadcfe116a51ab95feaa0f881cb2be65edd2547
xiaoouWANG <xiaoouwangfrance@gmail.com>
2019-06-13 09:46:16 +0200

    Updates

---
+++ precache-manifest.12efee867431e094dc5a4629e9d02d06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ef51123a0e536277ecb5960fd3e6c29",
+    "url": "/nasalpedia/index.html"
+  },
+  {
+    "revision": "49d7621609ca0dc8a0ae",
+    "url": "/nasalpedia/static/css/main.fc22b7e6.chunk.css"
+  },
+  {
+    "revision": "62ae393906c0b46ac502",
+    "url": "/nasalpedia/static/js/2.de3790c2.chunk.js"
+  },
+  {
+    "revision": "49d7621609ca0dc8a0ae",
+    "url": "/nasalpedia/static/js/main.4587659e.chunk.js"
+  },
+  {
+    "revision": "c6eee220b096728d79e1",
+    "url": "/nasalpedia/static/js/runtime~main.0c51de88.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/phoneticae/precache-manifest.1c082a33cd5ede556017326576dcd0b8.js"
+  "/nasalpedia/precache-manifest.12efee867431e094dc5a4629e9d02d06.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/phoneticae/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nasalpedia/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
